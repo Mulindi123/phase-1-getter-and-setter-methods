335434621beb9bfcd73b2a4c27c24937f3b65439
Medrine Musabi Mulindi <medrine.mulindi@student.moringaschool.com>
2023-08-13 11:43:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your Circle class here
+class Circle{
+    constructor(radius){
+        this.radius = radius;
+        this.pi = Math.PI;
+    }
+
+    get diameter(){
+        return this.radius *2;
+    }
+    get circumference(){
+        return 2*this.radius* this.pi;
+    }
+    get area(){
+        return this.radius*this.radius*this.pi;
+    }
+    set diameter(value){
+        return this.radius =value/2;
+    }
+    
+    set circumference(value){
+     return this.radius = value/(2* this.pi);
+        
+    }
+
+    set area(value){
+        return this.radius =Math.sqrt(value/this.pi);
+    }
+}
+
+let circle = new Circle(7)
